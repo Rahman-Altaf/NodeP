c83290e20a4bb3f897a10b5a941b2c295b488bd6
Rahman-Altaf <rahmanch.311@gmail.com>
2024-08-06 23:29:02 +0500

    New Functions / Require/ module.exports

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,5 @@
-const { sum, sub } = require("./maths");
+const { sum, sub, mul , div } = require("./maths");
 console.log("My Value Is ", sum(10, 22));
 console.log("My Value Is ", sub(10, 22));
+console.log("My Value Is ", mul(10, 22));
+console.log("My Value Is ", div(10, 22));
