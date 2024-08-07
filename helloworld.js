8d826944440b5fb0b5c5ef76032040d9bef7764d
Rahman Altaf <157910357+Rahman-Altaf@users.noreply.github.com>
2024-08-08 01:14:52 +0500

    Delete helloworld.js
    Raw File

--- helloworld.js
+++
@@ -1,5 +0,0 @@
-const { sum, sub, mul , div } = require("./maths");
-console.log("My Value Is ", sum(10, 22));
-console.log("My Value Is ", sub(10, 22));
-console.log("My Value Is ", mul(10, 22));
-console.log("My Value Is ", div(10, 22));
